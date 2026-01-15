ca2b3925adf67dcd940c96b3be19b8aafc9bf750
esraarabee1 <esraarabee69@gmail.com>
2026-01-15 05:48:57 -0800

    i added second section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import Feature from "./components/Feature";
 
 const App = () => {
   return (
     <div className="">
       <Header />
       <HeroSection />
+      <Feature />
     </div>
   );
 };
---
+++ src/components/Feature.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import f1 from "../assets/f1.png";
+import spanicon from "../assets/spanicon.png";
+
+const Feature = () => {
+  return (
+    <section className="bg-[#26262B] text-white py-24">
+      <div className="max-w-6xl mx-auto px-6">
+        {/* Header */}
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between mb-12">
+          <h2 className="text-2xl md:text-3xl font-semibold">
+            What you get on Setapp.
+          </h2>
+
+          <p className="text-gray-400 mt-4 md:mt-0 text-sm md:text-base">
+            With a single monthly subscription at $9.99, you get 240+ apps for
+            <br className="hidden md:block" />
+            your Mac.
+          </p>
+        </div>
+
+        {/* Feature Card */}
+        <div
+          className="bg-gradient-to-br from-[#f1a7bc] to-[#7b4f9d] rounded-2xl p-8 md:p-12 flex flex-col items-start gap-10
+"
+        >
+          <div className="flex-1">
+            <div className="flex items-center gap-4 mb-6">
+              <img src={spanicon} alt="icon" className="w-10 h-10" />
+              <div>
+                <h3 className="text-lg font-semibold text-black">
+                  Keep your Mac clean
+                </h3>
+                <p className="text-sm text-black/80">
+                  Remove junk, scan for malware, wipe email attachments
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/*  image */}
+          <div className="flex-1 flex justify-center">
+            <img
+              src={f1}
+              alt="feature"
+              className="max-w-full rounded-xl shadow-xl"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Feature;
